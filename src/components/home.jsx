90c1ff466d28ad5fc4ba97c82bf5a27e54e31297
harsha1238 <harsha882977@gmail.com>
2023-09-19 23:13:41 +0530

    style

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -147,7 +147,7 @@ let count=res.map((item,index)=>{
                 </textarea>
             </div>
         </div> : <><h3>No Data Found</h3>
-                <h3 style={{marginTop:"200px"}}>{easyComp()}</h3>
+                <h3 style={{marginTop:"250px"}}>{easyComp()}</h3>
         </>}
     </> 
 }
